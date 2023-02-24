5f4c8a8c9a00f86fec5bd232050f57a2ca1c83c5
Rijaia <zaya_89@mail.ru>
2023-02-24 18:29:51 +0300

    first iteration

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,20 @@
 import org.w3c.dom.ls.LSOutput;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Homework 5");
-        System.out.println("Задача 1");
-
-        int clintDeviceYear = 2014;
-        int clientOS = 1;
-
-        if (clintDeviceYear < 2015) {
-            if (clientOS == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    public static void printLeapYear(int leapYear){
+        System.out.println(leapYear+" високосный год");
+    }
+    public static void calculateLeapYear (int year){
+            if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ) {
+                printLeapYear(year);
             } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                System.out.println(year+" не високосный год");
             }
-        } else if (clientOS ==1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для iOS по ссылке");}
-        System.out.println("Задача 3");
-        int year = 900;
-        if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ) {
-            System.out.println("Год високосный");
-        } else {
-        System.out.println("Год не високосный");}
-
-        System.out.println("Задача 4");
-        int deliveryDistance =300;
+    }
+    public static void deliverInDay(int deliveryDistance){
         int time = 1;
         if (deliveryDistance < 20) {
             time = 1;
@@ -43,34 +29,24 @@ public class Main {
         if (time == -1) {
             System.out.println("Доставка не возможна");
         } else {
-         System.out.println("Срок доставки " +time+ " дней");}
+            System.out.println("Срок доставки " +time+ " дней");}
+    }
+    public static void installSystem (int clintDeviceYear, int clientOS){
+        if (clintDeviceYear < 2015) {
+            if (clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");}
 
-        System.out.println("Задача 5");
-        int monthNomber = 3;
-        switch (monthNomber){
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Этот месяц относится к зиме");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Этот месяц относится к весне");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Этот месяц относится к лету");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Этот месяц относится к осени");
-                break;
-            default:
-                System.out.println("Такого месяца не существует");
-        }
-
+    }
+    public static void main(String[] args) {
+        calculateLeapYear(1996);
+        installSystem(2010, 1);
+        deliverInDay(3);
     }
 }
